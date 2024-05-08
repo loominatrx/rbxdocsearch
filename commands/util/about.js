8bd9b10174eda2edf59889caf60e00f4902461cd
Mas Gading <loominatr1x@gmail.com>
2024-05-08 15:06:55 +0700

    `about.js`: add creator info

--- commands/util/about.js
+++ commands/util/about.js
@@ -8,6 +8,9 @@ this bot is open-sourced under [GPLv3 license](https://github.com/loominatrx/rbx
 its code sample that is shown in the documentation is used under [MIT license](https://github.com/Roblox/creator-docs/blob/main/LICENSE-CODE).
 `;
 
+const AUTHOR_BIO = `hello! i'm Mas Gading. people on the internet know me as [loominatrx](https://www.roblox.com/users/1565283543/profile).
+i'm a teenager, a roblox game developer, a gamer, and also a [6 digit osu! player](https://osu.ppy.sh/u/loominatrx). i do some lua(u) and javascript programming (i'm no expert at both of them), and also i do UI Design on roblox & figma.`;
+
 export default {
 	data: new SlashCommandBuilder()
 		.setName('about')
@@ -21,7 +24,7 @@ export default {
 		const minutes = Math.floor(totalSeconds / 60);
 		const seconds = Math.floor(totalSeconds % 60);
 
-		const embed = new EmbedBuilder()
+		const aboutBot = new EmbedBuilder()
 			.setTitle(BOT_NAME)
 			.setThumbnail('https://github.com/loominatrx/rbxdocsearch/blob/main/assets/profile-dark.png?raw=true')
 			.setDescription(DESCRIPTION)
@@ -47,8 +50,41 @@ export default {
 			.setFooter({
 				text: 'Made with <3 by loominatrx.',
 			});
+		const aboutCreator = new EmbedBuilder()
+			.setAuthor({
+				name: 'loominatrx',
+				iconURL: 'https://avatars.githubusercontent.com/u/35314624?v=4',
+			})
+			.setTitle('about me!')
+			.setDescription(AUTHOR_BIO)
+			.addFields(
+				{
+					name: 'trakteer me!',
+					value: 'by supporting me [here](https://trakteer.id/loominatrx/tip), you helped my financial situation for the better!',
+				},
+				{
+					name: 'twitter.',
+					value: '[@rbxlx_](https://twitter.com/rbxlx_)',
+					inline: true,
+				},
+				{
+					name: 'roblox.',
+					value: '[Loominatrx](https://user.rblx.name/Loominatrx)',
+					inline: true,
+				},
+				{
+					name: 'devforum.',
+					value: '[Loominatrx](https://user.devforum.link/Loominatrx)',
+					inline: true,
+				},
+				{
+					name: 'github.',
+					value: '[loominatrx](https://github.com/loominatrx)',
+					inline: true,
+				},
+			);
 		await interaction.reply({
-			embeds: [embed],
+			embeds: [aboutBot, aboutCreator],
 		});
 	},
 };
