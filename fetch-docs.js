36fc92df1ce5dc0be7ecb5920e835d30003271e3
Mas Gading <loominatr1x@gmail.com>
2024-05-08 15:05:08 +0700

    `fetch-docs.js`: download globals and libraries

--- fetch-docs.js
+++ fetch-docs.js
@@ -8,8 +8,13 @@ const header = {
 	'X-GitHub-Api-Version': '2022-11-28',
 };
 
-const filepath = path.join(path.dirname(Bun.main), 'references', 'engine', 'classes');
-fs.mkdirSync(filepath, { recursive: true });
+const instance_path = path.join(path.dirname(Bun.main), 'references', 'engine', 'classes');
+const globals_path = path.join(path.dirname(Bun.main), 'references', 'engine', 'globals');
+const libraries_path = path.join(path.dirname(Bun.main), 'references', 'engine', 'libraries');
+
+fs.mkdirSync(instance_path, { recursive: true });
+fs.mkdirSync(globals_path, { recursive: true });
+fs.mkdirSync(libraries_path, { recursive: true });
 
 axios.request({
 	baseURL: 'https://api.github.com/repos/Roblox/creator-docs/contents',
@@ -17,7 +22,6 @@ axios.request({
 	header: header,
 	responseType: 'json',
 }).then((response) => {
-	console.log('Get response!');
 	const files = response.data;
 	for (const data of files) {
 		const filename = data.name;
@@ -27,8 +31,50 @@ axios.request({
 			header: header,
 			responseType: 'blob',
 		}).then((blob) => {
-			fs.writeFileSync(path.join(filepath, filename), blob.data);
-			console.log(`File saved at ${path.join(filepath, filename)}`);
+			fs.writeFileSync(path.join(instance_path, filename), blob.data);
+			console.log(`File saved at ${path.join(instance_path, filename)}`);
+		});
+	}
+});
+
+axios.request({
+	baseURL: 'https://api.github.com/repos/Roblox/creator-docs/contents',
+	url: '/content/en-us/reference/engine/globals',
+	header: header,
+	responseType: 'json',
+}).then((response) => {
+	const files = response.data;
+	for (const data of files) {
+		const filename = data.name;
+		const download_url = data.download_url;
+		axios.request({
+			url: download_url,
+			header: header,
+			responseType: 'blob',
+		}).then((blob) => {
+			fs.writeFileSync(path.join(globals_path, filename), blob.data);
+			console.log(`File saved at ${path.join(globals_path, filename)}`);
+		});
+	}
+});
+
+axios.request({
+	baseURL: 'https://api.github.com/repos/Roblox/creator-docs/contents',
+	url: '/content/en-us/reference/engine/libraries',
+	header: header,
+	responseType: 'json',
+}).then((response) => {
+	const files = response.data;
+	for (const data of files) {
+		const filename = data.name;
+		const download_url = data.download_url;
+		axios.request({
+			url: download_url,
+			header: header,
+			responseType: 'blob',
+		}).then((blob) => {
+			fs.writeFileSync(path.join(libraries_path, filename), blob.data);
+			console.log(`File saved at ${path.join(libraries_path, filename)}`);
 		});
 	}
 });
